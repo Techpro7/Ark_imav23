41587d585d9003dff23ca37e82e7eac0c48522e6
sd-drago <shriramdeshpande2003@gmail.com>
2023-07-08 00:16:20 +0530

    Package for Simulation

---
+++ imav_2023/imav_plugins/src/build/CMakeFiles/rotation_controller.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rotation_controller.dir/rotation_controller.cpp.o"
+  "librotation_controller.pdb"
+  "librotation_controller.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rotation_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ imav_2023/imav_plugins/src/rotation_controller.cpp
@@ -0,0 +1,26 @@
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+
+namespace gazebo
+{
+  class RotationControllerPlugin : public ModelPlugin
+  {
+  public:
+    void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+    {
+      // Check if the model has a joint named "joint_name"
+      physics::JointPtr joint = _model->GetJoint("link_1_JOINT_0");
+      if (!joint)
+      {
+        gzerr << "Joint not found: joint_name" << std::endl;
+        return;
+      }
+
+      // Apply effort to the joint to rotate the platform
+      joint->SetForce(0, 10);
+    }
+  };
+
+  GZ_REGISTER_MODEL_PLUGIN(RotationControllerPlugin)
+}
